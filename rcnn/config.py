bb94ac975879c6121939da32534dc36fc324c1cb
xkl265 <xkl265@163.com>
2024-10-29 12:14:42 +1100

    update config

--- rcnn/config.py
+++ rcnn/config.py
@@ -1,5 +1,7 @@
-
+import json
 class DefaultConfig:
   # data
-  image_dir = 'VOC2012/train/JPEGImages'
-  annotation_dir = 'VOC2012/train/Annotations'
+  # image_dir = 'VOC2012/train/JPEGImages'
+  # annotation_dir = 'VOC2012/train/Annotations'
+  with open('classes.json', 'r') as f:
+    classes = json.load(f)
